c300a0437551512732c35b9f3e823ce4b2cd82b0
TomNdPop <lilleymanryan2020@gmail.com>
2023-03-10 18:26:06 -0500

    Add the second exercise solution for TSS and JS

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    var result = '';
+    if(num >= 0){
+        for(var i = 0; i < num; i++){
+            result += string;
+        }
+    }else if(num < 0){
+        result = "ERROR"
+    }
+    return result;
+    
 };
 
 // Do not edit below this line
