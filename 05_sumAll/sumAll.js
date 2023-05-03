b3ff99d02c0677a71d13fa5c8b9918b2fd12afb1
TomNdPop <lilleymanryan2020@gmail.com>
2023-05-02 20:00:39 -0400

    05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    result=0;
+    if(typeof(min) != 'number'||typeof(max)!='number'){
+        return 'ERROR'
+    }else if(min > 0 && max > 0){
+        if(min<max){
+            for(var i = min;i<=max;i++){
+                result+=i;
+            }
+        }else if(min>max){
+            for(var i = max;i<=min;i++){
+                result+=i;
+            }
+        }
+    }else if(min < 0 || max < 0){
+        return 'ERROR';
+    }
 
+    return result;
 };
 
 // Do not edit below this line
